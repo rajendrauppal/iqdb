c3d1f24960ebeab9a036e6dcafa37709409ef7c8
Rajendra Uppal <rajen.iitd@gmail.com>
2018-08-07 00:01:30 +0530

    complete get single question, get all questions, update a question and delete a question APIs

--- app/controllers/question.controller.js
+++ app/controllers/question.controller.js
@@ -6,7 +6,7 @@ const Question = require('../models/question.model');
 
 // create and save a new question
 exports.create = (req, res) => {
-  console.log("create a question");
+
   // validate request
   if (!req.body) {
     res.status(400).send({
@@ -18,11 +18,6 @@ exports.create = (req, res) => {
   const question = new Question({
     statement: req.body.statement,
     solutions: req.body.solutions,
-    // solutions: [{
-    //   solution: req.body.solution,
-    //   timeComplexity: req.body.timeComplexity,
-    //   spaceComplexity: req.body.spaceComplexity
-    // }],
     difficultyLevel: req.body.difficultyLevel,
     category: req.body.category,
     subCategory: req.body.subCategory
@@ -35,7 +30,7 @@ exports.create = (req, res) => {
     })
     .catch(err => {
       res.status(500).send({
-        message: err.message || "Some error occurred in saving the Question."
+        message: err.message || "Some error occurred while saving the Question."
       });
     });
 
@@ -44,20 +39,109 @@ exports.create = (req, res) => {
 // fetch and return all questions from mongodb
 exports.findAll = (req, res) => {
 
+  Question.find()
+    .then(questions => {
+      res.send(questions);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while fetching all questions."
+      });
+    });
+
 }
 
 // fetch and return a single question
 exports.findOne = (req, res) => {
+
+  Question.findById(req.params.questionId)
+    .then(question => {
+      if (!question) {
+        res.status(404).send({
+          message: "Question not found with id " + req.params.questionId
+        });
+      }
+      res.send(question);
+    })
+    .catch(err => {
+      if (err.kind === 'ObjectId') {
+        res.status(404).send({
+          message: "Question not found with id " + req.params.questionId
+        });
+      }
+      res.send(500).send({
+        message: "Question not found with id " + req.params.questionId
+      });
+    });
 
 }
 
 // update a single question
 exports.update = (req, res) => {
 
+  // validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Input cannot be empty"
+    });
+  }
+
+  // find the question and update it with the request body
+  Question.findByIdAndUpdate(
+    req.params.questionId,
+    {
+      statement: req.body.statement,
+      solutions: req.body.solutions,
+      difficultyLevel: req.body.difficultyLevel,
+      category: req.body.category,
+      subCategory: req.body.subCategory
+    },
+    {
+      new: true
+    })
+  .then(question => {
+    if (!question) {
+      res.status(404).send({
+        message: "Question not found with id " + req.params.questionId
+      });
+    }
+    res.send(question);
+  })
+  .catch(err => {
+    if (err.kind === 'ObjectId') {
+      res.status(404).send({
+        message: "Question not found with id " + req.params.questionId
+      });
+    }
+    res.send(500).send({
+      message: "Error updating question with id " + req.params.questionId
+    });
+  });
+
 }
 
 // delete a single question
 exports.delete = (req, res) => {
+
+  Question.findByIdAndRemove(req.params.questionId)
+    .then(question => {
+      if (!question) {
+        res.status(404).send({
+          message: "Question not found with id " + req.params.questionId
+        });
+      }
+      res.send({message: "Question deleted successfully."});
+    })
+    .catch(err => {
+      if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+        res.status(404).send({
+          message: "Question not found with id " + req.params.questionId
+        });
+      }
+      res.send(500).send({
+        message: "Could not delete question with id " + req.params.questionId
+      });
+    });
 
 }
 
