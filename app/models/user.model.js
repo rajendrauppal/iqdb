7a2841e0bf2daf20360c65981a7e148fb8ae0fa1
Rajendra Uppal <rajen.iitd@gmail.com>
2018-08-09 23:34:14 +0530

    complete user model

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,1 +1,17 @@
+
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+  {
+    name: String,
+    password: String,
+    admin: Boolean
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('User', userSchema);
+
   
